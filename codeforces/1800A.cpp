209361d05be7bda40230deb21ee1713cd9594eeb
Alam Sanchez <asancheztorres1905@gmail.com>
2023-03-02 10:51:21 -0600

    Codeforces Round 855

---
+++ codeforces/1800A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int t, n, next;
+    string s, meow = "meow";
+    cin >> t;
+
+    while (t--) {
+        cin >> n >> s;
+        next = 0;
+        char curr = tolower(s[0]), prev = curr;
+        bool pos = curr == meow[0];
+
+        for (int i = 1; i < n && pos; i++) {
+            curr = tolower(s[i]);
+            if (curr != prev) {
+                // cout << "-> " << curr << " " << prev << endl;
+                next++;
+            }
+            // cout << "cmp " << curr << " " << meow[next] << endl;
+            if (next > 3 || curr != meow[next]) {
+                pos = false;
+                break;
+            }
+            prev = curr;
+        }
+
+        pos = pos && (next == 3);
+
+        cout << (pos ? "YES" : "NO") << endl;
+    }
+}
